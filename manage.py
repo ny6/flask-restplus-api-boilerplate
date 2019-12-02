5c054af276caf7d64335059b15e4e20008182a5b
Yeshu Kochher <yeshu@outlook.in>
2019-12-02 20:04:24 +0530

    one where blacklist token model and service added

--- manage.py
+++ manage.py
@@ -5,7 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from app.main import create_app, db
-from app.main.model import user
+from app.main.model import user, blacklist
 from app import blueprint
 
 app = create_app(os.getenv('FLASK_ENV', 'development'))
