9d67c2ec3e1ca05a8f15d533d7655fd505e2e19e
Yeshu Kochher <yeshu@outlook.in>
2019-12-02 18:23:31 +0530

    one where user model added

--- manage.py
+++ manage.py
@@ -5,6 +5,7 @@ from flask_migrate import Migrate, MigrateCommand
 from flask_script import Manager
 
 from app.main import create_app, db
+from app.main.model import user
 
 app = create_app(os.getenv('FLASK_ENV', 'development'))
 
