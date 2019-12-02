a09ecfcc28b372a4efb4800ab9a585dd8d8eead5
Yeshu Kochher <yeshu@outlook.in>
2019-12-02 18:33:39 +0530

    one where config test cases added

---
+++ app/test/test_config.py
@@ -0,0 +1,43 @@
+import os
+import unittest
+
+from flask import current_app
+from flask_testing import TestCase
+
+from manage import app
+from app.main.config import basedir
+
+
+class TestDevelopmentConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('app.main.config.DevelopmentConfig')
+        return app
+
+    def test_app_is_development(self):
+        self.assertFalse(app.config['SECRET_KEY'] is 'invalid_key')
+        self.assertTrue(app.config['DEBUG'] is True)
+        self.assertFalse(current_app is None)
+
+
+class TestTestingConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('app.main.config.TestingConfig')
+        return app
+
+    def test_app_is_testing(self):
+        self.assertFalse(app.config['SECRET_KEY'] is 'invalid_key')
+        self.assertTrue(app.config['DEBUG'])
+        self.assertTrue(app.config['SQLALCHEMY_DATABASE_URI'] == f"sqlite:///{os.path.join(basedir, 'test.db')}")
+
+
+class TestProductionConfig(TestCase):
+    def create_app(self):
+        app.config.from_object('app.main.config.ProductionConfig')
+        return app
+
+    def test_app_is_production(self):
+        self.assertTrue(app.config['DEBUG'] is False)
+
+
+if __name__ == '__main__':
+    unittest.main()
